314f043f94820a1f6782089ddb63a4bdb76028e9
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-16 23:14:37 +0530

    let,const and var

---
+++ 03_diff_let_var_const.js
@@ -0,0 +1,22 @@
+var a = 5;
+
+var b = "Krishna";
+
+{
+  var b = "Yash";
+  console.log(b);
+}
+// var can introduce errors in your code
+console.log(b);
+let c = "Krishna";
+
+{
+  let c = "Yash";
+  console.log(c);
+}
+
+console.log(c);
+
+const author = "Avdhesh";
+
+let author = "kartik";
