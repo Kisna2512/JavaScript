e836c7b8a6b7dbeb23d0ea7515ea1466603b44ac
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-17 21:11:06 +0530

    Primitives and objects in javaScript

---
+++ 04_primitive_objects.js
@@ -0,0 +1,25 @@
+// primitives in js
+let a = 4;
+let b = true;
+let c = BigInt("666") + BigInt("4");
+let d = "krishna";
+let e = null;
+let f;
+console.log(a, b, c, d);
+console.log(typeof d);
+console.log(typeof c);
+console.log(typeof a);
+console.log(typeof e);
+console.log(typeof f);
+
+// objects in js
+
+const item = {
+  Krishna: 100,
+  Yash: 200,
+  kartik: 300,
+  Shubham: 400,
+};
+
+console.log(item["Krishna"]);
+console.log(item["Aniket"]);
