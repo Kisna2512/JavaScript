05c1531ac6cf227721d8618daa5ccb245120e86e
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-19 23:48:13 +0530

    Condtional in JS

---
+++ 07_condtional_statement.js
@@ -0,0 +1,9 @@
+let a = prompt("Hewy What's your age?");
+a = Number.parseInt(a);
+if (a > 45) {
+  console.log("You are old");
+} else if (a > 20) {
+  alert("You are too young");
+} else {
+  console.log("You are young");
+}
