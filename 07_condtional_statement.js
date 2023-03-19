a9efd70d8ff7389b472d18a9e13039521f60d014
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-19 23:54:05 +0530

    Switch case in JS

--- 07_condtional_statement.js
+++ 07_condtional_statement.js
@@ -1,9 +1,21 @@
-let a = prompt("Hewy What's your age?");
-a = Number.parseInt(a);
-if (a > 45) {
-  console.log("You are old");
-} else if (a > 20) {
-  alert("You are too young");
-} else {
-  console.log("You are young");
+// let a = prompt("Hewy What's your age?");
+// a = Number.parseInt(a);
+// if (a > 45) {
+//   console.log("You are old");
+// } else if (a > 20) {
+//   alert("You are too young");
+// } else {
+//   console.log("You are young");
+// }
+
+let a = "Krishna";
+
+switch (a) {
+  case "Krishna":
+    console.log("This is Krishna");
+    break;
+  case "Yash":
+    console.log("this is Yash");
+  default:
+    break;
 }
