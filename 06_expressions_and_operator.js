950403b925e5998703d776d6bb57c46824e2cffe
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-19 17:13:38 +0530

    Operators in JavaScript

---
+++ 06_expressions_and_operator.js
@@ -0,0 +1,29 @@
+let a = 45;
+let b = 4;
+// Mathematical operators
+console.log("a+b =", a + b);
+console.log("a-b =", a - b);
+console.log("a/b =", a / b);
+console.log("a*b =", a * b);
+console.log("a^b =", a ** b);
+console.log("a%b =", a % b);
+console.log(a++);
+console.log(a--);
+
+// Assignment operator in js =
+a = 5;
+a -= 5;
+a += 5;
+a *= 5;
+a /= 5;
+console.log(a);
+
+// comparison operators in js-: === ,==, >= , <= ,> ,<
+let a1 = 4;
+let b1 = 4;
+
+if (a1 === b1) {
+  console.log("They are equal");
+}
+
+// Logical operators
