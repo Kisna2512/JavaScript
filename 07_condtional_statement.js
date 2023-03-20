154d9271e773f103ee44398311c9a8c23359710e
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-21 01:00:39 +0530

    Types of for loop

--- 07_condtional_statement.js
+++ 07_condtional_statement.js
@@ -1,12 +1,12 @@
-// let a = prompt("Hewy What's your age?");
-// a = Number.parseInt(a);
-// if (a > 45) {
-//   console.log("You are old");
-// } else if (a > 20) {
-//   alert("You are too young");
-// } else {
-//   console.log("You are young");
-// }
+let a = prompt("Hewy What's your age?");
+a = Number.parseInt(a);
+if (a > 45) {
+  console.log("You are old");
+} else if (a > 20) {
+  alert("You are too young");
+} else {
+  console.log("You are young");
+}
 
 let a = "Krishna";
 
---
+++ 08_for_loop.js
@@ -0,0 +1,13 @@
+for (let i = 0; i <= 34; i++) {
+  console.log(i);
+}
+
+let arr = [99, 100, 101];
+
+for (let marks in arr) {
+  console.log(arr[marks]);
+}
+
+for (let b of arr) {
+  console.log(b);
+}
