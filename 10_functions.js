3c22476be5b071c7eda3ab6cfcbafbb519fa6f60
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-24 18:20:48 +0530

    Functions in JavaScript

---
+++ 10_functions.js
@@ -0,0 +1,17 @@
+function avg(a, b) {
+  console.log("Done");
+  return (a + b) / 2;
+}
+
+const hello = () => {
+  console.log("Hello World!!");
+};
+
+let a = 2;
+let b = 4;
+console.log(avg(a, b));
+
+const sum = (a, b) => a + b;
+
+console.log(sum(a, b));
+hello();
