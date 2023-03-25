59cc141348d897182cb31787173d2199fff8adfe
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-25 16:08:27 +0530

    String Slicing method

--- 12_string.js
+++ 12_string.js
@@ -12,3 +12,7 @@ console.log(fruit);
 
 console.log(boy1.toLowerCase());
 console.log(boy2.toUpperCase());
+
+console.log(boy1.slice(2));
+console.log(boy1.slice(1, 2));
+console.log(boy2.slice(2, 5));
