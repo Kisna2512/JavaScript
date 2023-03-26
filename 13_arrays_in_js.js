c988a14c719b5512c50b3f73a43bfb3dc0c0ddd7
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-26 16:16:16 +0530

    Array Methods:toString

--- 13_arrays_in_js.js
+++ 13_arrays_in_js.js
@@ -3,3 +3,10 @@ console.log(marks);
 console.log(marks[1]);
 marks.push(67);
 console.log(marks.length);
+
+// Array methods
+
+let a = [23, 45, 32, 12];
+let b = a.toString();
+console.log(b);
+console.log(typeof b);
