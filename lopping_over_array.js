8b960366c2b39b30c66e8032980ff455b727733f
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-28 14:04:33 +0530

    for in loop over array

--- lopping_over_array.js
+++ lopping_over_array.js
@@ -1,9 +1,20 @@
 let num = [3, 54, 1, 2, 4];
 
-for (let i = 0; i < num.length; i++) {
-  console.log(num[i]);
-}
+/// Simple for loop
+// for (let i = 0; i < num.length; i++) {
+//   console.log(num[i]);
+// }
 
 num.forEach((element) => {
-  console.log(element);
+  console.log(element * element);
 });
+
+// for of
+for (let ele of num) {
+  console.log(ele);
+}
+
+// for in
+for (const key in num) {
+  console.log(num[key]);
+}
