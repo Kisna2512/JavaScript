b917bff740152ff790533d7ccc0ab6243000f7e0
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-01-01 22:36:51 +0530

    Variables in JS

---
+++ 01_basics/Variables.js
@@ -0,0 +1,18 @@
+const accountId = 145654;
+let accountEmail = "krishna@gmail.com";
+var accountPassword = "12345";
+accountCity = "Pune";
+console.log(accountId);
+let accountDate;
+
+accountEmail = "kartik@gmail.com";
+accountPassword = "34322";
+accountCity = "Nashik";
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountDate,
+]);
