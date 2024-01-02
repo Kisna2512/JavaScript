5e7cbf68fd766db44fdf36b78c1fee02b84d077d
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-01-02 20:56:11 +0530

    datatypes in JS

---
+++ 01_basics/DataTypes.js
@@ -0,0 +1,12 @@
+let name = "Krishna"; // String
+let age = 21; // int
+let isLoggedin = false; // boolean
+// null -standalone value
+// undefined -default value type
+// symbol - unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null); //output : object
+console.log(typeof undefined); //output : undefined
