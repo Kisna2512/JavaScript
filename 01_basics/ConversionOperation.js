69e0c223c55b54f0a8f80f4579f700319f96c5a9
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-01-03 21:53:33 +0530

    Conversion of datatypes into another type in JS

---
+++ 01_basics/ConversionOperation.js
@@ -0,0 +1,17 @@
+let score = "23abc";
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1;
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+
+let someNumber = 44;
+let stringNumber = String(someNumber);
+console.log(someNumber);
+console.log(typeof stringNumber);
