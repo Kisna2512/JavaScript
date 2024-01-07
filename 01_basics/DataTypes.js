fc7803aea1407057f3a4bba538fe398706d94288
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-01-07 16:24:23 +0530

    Updated Datatypes code

--- 01_basics/DataTypes.js
+++ 01_basics/DataTypes.js
@@ -1,3 +1,4 @@
+// Primitive data types: Number,String,undefined,null,Symbol,BigInt,boolean
 let name = "Krishna"; // String
 let age = 21; // int
 let isLoggedin = false; // boolean
@@ -7,6 +8,29 @@ let isLoggedin = false; // boolean
 
 // object
 
+// symbols
+const ID = Symbol("123");
+const anotherId = Symbol("123");
+console.log(ID === anotherId);
+// bigNumber
+const bigNumber = 4234354524324235345n;
+console.log(bigNumber);
 console.log(typeof age);
 console.log(typeof null); //output : object
 console.log(typeof undefined); //output : undefined
+
+// Non -primitive
+
+// Array,objects,functions
+const myArray = [123, 456, 3221, 567];
+let myObj = {
+  name: "krishna",
+  age: 22,
+};
+
+console.log(myObj);
+
+const myfun = function () {
+  console.log("hello World");
+};
+console.log(typeof ID);
