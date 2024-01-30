b6266456cbcc3b443ab45500174c54a062517873
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-01-30 17:35:41 +0530

    Switch in JavaScript

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,20 @@
+const month = 2;
+
+switch (month) {
+  case 1:
+    console.log("Jan");
+    break;
+  case 2:
+    console.log("Feb");
+    break;
+  case 3:
+    console.log("Mar");
+    break;
+  case 4:
+    console.log("Apr");
+    break;
+
+  default:
+    console.log("No Valid Months");
+    break;
+}
