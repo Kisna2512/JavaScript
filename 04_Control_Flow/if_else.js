ccb87d6a8dc5d43297e7af42a8233991fcc23ad9
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-01-31 13:25:10 +0530

    Nullish Coalescing Operator in JS

--- 04_Control_Flow/if_else.js
+++ 04_Control_Flow/if_else.js
@@ -21,3 +21,20 @@ if (temp < 50) {
 const bal = 400;
 // if (bal < 500) console.log("bal is less than 500"), console.log("Tested!!");
 console.log(bal > 100 ? "Grater than 100" : "Less than 100");
+//
+
+// Nullish Coalescing Operator(??): null undefined
+
+let val1 = null ?? "Email Not found";
+val1 = undefined ?? 15;
+console.log(val1);
+
+let val2 = null ?? undefined ?? null;
+console.log(val2);
+
+// Terniary  Operator
+
+const Price = 100;
+Price >= 100
+  ? console.log("Price greater than 100")
+  : console.log("Less than 100");
