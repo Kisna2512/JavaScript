1b05619caaae9a29b5659c73134b27de9fa1ec46
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-02-02 17:03:15 +0530

    While and do while loop in JS

---
+++ 05_iterations/whileanddowhile.js
@@ -0,0 +1,19 @@
+let idx = 0;
+while (idx <= 10) {
+  console.log(`Value of index is ${idx}`);
+  idx++;
+}
+
+let arr = ["Krishna", "Kartik", "Shubham", "Yash"];
+
+let i = 0;
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
+}
+
+let score = 1;
+do {
+  console.log(`Score is ${score}`);
+  score = score + 2;
+} while (score <= 10);
