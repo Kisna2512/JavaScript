b59366c1397181b22a0178ade4a3df739565d506
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-02-07 18:42:18 +0530

    Map,filter and Reduce in JS

--- 05_iterations/MapFilterReduce.js
+++ 05_iterations/MapFilterReduce.js
@@ -28,3 +28,24 @@ const myArrOfObjects = [
 
 const result = myArrOfObjects.filter((obj) => obj.age >= 22);
 console.log(result);
+
+const myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const myArr1 = myArr.map((num) => num + 10);
+
+// chaining
+const myArr2 = myArr
+  .map((num) => num * 10)
+  .map((num) => num + 1)
+  .filter((num) => num > 20);
+console.log(myArr2);
+
+const myArr3 = [1, 2, 3];
+const myTotal = myArr3.reduce((acc, cur) => acc + cur, 0);
+console.log(myTotal);
+
+const myArr4 = [1, 2, 3, 4, 5];
+const total = myArr4
+  .map((num) => num + 1)
+  .filter((num) => num % 2 == 0)
+  .reduce((acc, num) => acc + num, 0);
+console.log(`Total is: ${total}`);
