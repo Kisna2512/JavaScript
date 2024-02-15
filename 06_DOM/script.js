2635b5eddf2e7a1541b5a1494657c9ad0b7bc32b
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-02-15 12:47:50 +0530

    DOM selectors in JS

--- 06_DOM/script.js
+++ 06_DOM/script.js
@@ -1,10 +1,22 @@
-const myul = document.querySelector("ul");
+// const myul = document.querySelector("ul");
 
-const tG = myul.querySelector("li");
+// const tG = myul.querySelector("li");
 
-tG.style.backgroundColor = "blue";
+// tG.style.backgroundColor = "blue";
 
-const templist = document.querySelectorAll("li");
-const tempClassList = document.getElementsByClassName("list-item");
-const myconvertedArray = Array.from(tempClassList);
-myconvertedArray.forEach((l) => console.log(l));
+// const templist = document.querySelectorAll("li");
+// const tempClassList = document.getElementsByClassName("list-item");
+// const myconvertedArray = Array.from(tempClassList);
+// myconvertedArray.forEach((l) => console.log(l));
+
+const parent = document.querySelector(".parent");
+console.log(parent);
+console.log(parent.children);
+console.log(parent.children[1].innerText);
+
+const newArray = Array.from(parent.children);
+newArray.forEach((item) => console.log(item.innerHTML));
+
+const dayOne = document.querySelector(".day");
+console.log(dayOne.nextElementSibling.innerHTML);
+console.log(dayOne.parentElement);
