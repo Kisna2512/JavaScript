dd6ab890c889a17be16cc094d344f53c185d059e
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-02-26 15:58:53 +0530

    Events in JS

--- 08_Events/eventbasics.js
+++ 08_Events/eventbasics.js
@@ -1,14 +1,14 @@
-// document.getElementById("owl").onclick = () => {
-//   alert("Owl Clicked");
-// };
+document.getElementById("owl").onclick = () => {
+  alert("Owl Clicked");
+};
 
-// document.getElementById("owl").addEventListener(
-//   "click",
-//   () => {
-//     alert("Owl CLicked Again!!");
-//   },
-//   false
-// );
+document.getElementById("owl").addEventListener(
+  "click",
+  () => {
+    alert("Owl CLicked Again!!");
+  },
+  false
+);
 document.getElementById("images").addEventListener(
   "click",
   () => {
@@ -16,11 +16,31 @@ document.getElementById("images").addEventListener(
   },
   false
 );
+
 document.getElementById("owl").addEventListener(
   "click",
   (e) => {
     console.log("Owl CLicked Again!!");
     e.stopPropagation();
+  },
+  false
+);
+
+document.getElementById("google").addEventListener("click", (e) => {
+  console.log("Clicked on Google");
+  e.preventDefault();
+});
+
+document.querySelector("#images").addEventListener(
+  "click",
+  (e) => {
+    console.log(e.target.parentNode);
+    if (e.target.tagName === "IMG") {
+      let removeIt = e.target.parentNode;
+      removeIt.remove();
+    }
+
+    // removeIt.parentNode.removeChild(removeIt);
   },
   false
 );
